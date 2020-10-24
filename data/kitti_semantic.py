5d2c7690569dba970d3e4650345bca2ddfe11839
Namrata Deka <namratadeka5@gmail.com>
2020-10-24 22:31:08 +0530

    crop image

--- data/kitti_semantic.py
+++ data/kitti_semantic.py
@@ -8,18 +8,20 @@ from torch.utils.data import Dataset
 
 
 class Kitti360Semantic(Dataset):
-	def __init__(self, data_dir:str, sample_size:int, rgb:bool, **ignored):
+	def __init__(self, data_dir:str, sample_size:int, rgb:bool, crop_size:int, **ignored):
 		if rgb:
 			self.data = glob(join(data_dir, '*', 'semantic_rgb', '*.png'))[:sample_size]
 		else:
 			self.data = glob(join(data_dir, '*', 'semantic', '*.png'))[:sample_size]
 		self.rgb = rgb
+		self.crop_size = crop_size
 
 	def __len__(self):
 		return len(self.data)
 
 	def __getitem__(self, index):
 		mask = cv2.imread(self.data[index])
+		mask = cv2.resize(mask, (self.crop_size, self.crop_size), interpolation=cv2.INTER_CUBIC)
 		if self.rgb:
 			mask = mask/255.
 		else:
