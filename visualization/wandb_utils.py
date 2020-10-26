084086eb6aab396d3488bf76ccc2d4bd09e92563
Namrata Deka <namratadeka5@gmail.com>
2020-10-27 01:49:51 +0530

    remove batchnorms to reduce stochasticity

--- visualization/wandb_utils.py
+++ visualization/wandb_utils.py
@@ -13,7 +13,7 @@ def init_wandb(cfg: dict) -> None:
         cfg[key] = cfg[key].__dict__
 
     wandb.init(project="scene-rearrangement", name=cfg["exp_cfg"]["run_name"], 
-    	notes=cfg["exp_cfg"]["description"], config=cfg)
+        notes=cfg["exp_cfg"]["description"], config=cfg)
 
 def log_epoch_summary(epochID:int, mode:str, losses:dict):
     logs = {}
@@ -23,11 +23,11 @@ def log_epoch_summary(epochID:int, mode:str, losses:dict):
     wandb.log(logs, step=epochID)
 
 def visualize_images(epochID, mode, gt_image, pred_image):
- 	grid = np.zeros_like(gt_image.cpu().numpy())
- 	for i in range(0, gt_image.shape[0], 2):
- 		grid[i] = gt_image[i].cpu().numpy()
- 		grid[i+1] = pred_image[i].detach().cpu().numpy()
+    grid = np.zeros_like(gt_image.cpu().numpy())
+    for i in range(0, gt_image.shape[0], 2):
+        grid[i] = gt_image[i].cpu().numpy()
+        grid[i+1] = pred_image[i].detach().cpu().numpy()
 
- 	grid = vutils.make_grid(torch.from_numpy(grid), nrow=2, normalize=True, scale_each=True)
+    grid = vutils.make_grid(torch.from_numpy(grid), nrow=2, normalize=True, scale_each=True)
 
- 	wandb.log({"{}_reconstruction".format(mode): wandb.Image(grid)}, step=epochID)
+    wandb.log({"{}_reconstruction".format(mode): wandb.Image(grid)}, step=epochID)
