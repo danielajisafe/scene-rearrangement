2d02bc5b67eb8366bab5fa97cec018264dcf7a6a
Namrata Deka <namratadeka5@gmail.com>
2020-11-02 16:40:48 +0530

    add back wandb.watch()

--- losses/losses.py
+++ losses/losses.py
@@ -23,9 +23,12 @@ def cross_entropy(x, y):
 	'''
 	return F.cross_entropy(x, y.squeeze(1).long())
 
-
 def KL(mu, log_var):
 	'''
 	KL divergence between N(mu, sigma) and N(0, 1)
 	'''
 	return torch.mean(-0.5 * torch.sum(1 + log_var - mu**2 - log_var.exp(), dim=1), dim=0)
+
+def elbo(mu, log_var, z, reconst, input):
+	# TODO
+	pass
