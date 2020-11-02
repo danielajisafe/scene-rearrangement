a727369d122044f983e6268de961211293ae51ac
Namrata Deka <namratadeka5@gmail.com>
2020-11-02 13:49:19 +0530

    make horizontal grid

--- visualization/wandb_utils.py
+++ visualization/wandb_utils.py
@@ -23,12 +23,12 @@ def log_epoch_summary(epochID:int, mode:str, losses:dict):
     wandb.log(logs, step=epochID)
 
 def visualize_images(epochID, mode, gt_image, pred_image):
-    grid = np.zeros_like(gt_image)
-    pred_image = np.expand_dims(pred_image.max(axis=1), 1)
-    for i in range(0, gt_image.shape[0], 2):
+    grid = np.zeros((8, gt_image.shape[1], gt_image.shape[2], gt_image.shape[3]))
+    pred_image = np.expand_dims(pred_image.argmax(axis=1), 1)
+    for i in range(grid.shape[0]//2):
         grid[i] = gt_image[i]
-        grid[i+1] = pred_image[i]
+        grid[i+4] = pred_image[i]
 
-    grid = vutils.make_grid(torch.from_numpy(grid), nrow=2, normalize=True, scale_each=True)
+    grid = vutils.make_grid(torch.from_numpy(grid), nrow=4, normalize=True, scale_each=True)
 
     wandb.log({"{}_reconstruction".format(mode): wandb.Image(grid)}, step=epochID)
