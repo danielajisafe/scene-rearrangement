fba62ab4b06ca116c8079839870f98ed1993ba36
ehedlin <iamerichedlin@gmail.com>
2020-11-06 09:05:27 -0800

    added new data loader

--- data/kitti_semantic.py
+++ data/kitti_semantic.py
@@ -41,3 +41,75 @@ class Kitti360SemanticBuilder(object):
             crop_size=crop_size
         )
         return self._instance
+
+class Kitti360Classes(Dataset):
+	def __init__(self, data_dir:str, sample_size:int, crop_size:int):
+		self.data = glob(join(data_dir, '*', 'semantic', '*.png'))
+		random.shuffle(self.data)
+		self.data = self.data[:sample_size]
+		self.crop_size = crop_size
+		self.num_classes = 45
+
+	def __len__(self):
+		return len(self.data)
+
+	def __getitem__(self, index):
+		image = cv2.imread(self.data[index])
+		image = cv2.resize(image, (self.crop_size, self.crop_size), interpolation=cv2.INTER_CUBIC)
+
+		image = torch.Tensor(image)
+
+		image = torch.mean(image, dim=-1)
+
+		ones = torch.ones(image.shape)
+		# ones = torch.ones((image.shape[0], image.shape[1], self.num_classes))
+		zeros = torch.zeros(image.shape)
+		# zeros = torch.zeros((image.shape[0], image.shape[1], self.num_classes))
+
+		binary_classification = torch.ones((image.shape[0], image.shape[1], self.num_classes))
+
+		for i in range(self.num_classes):
+			binary_classification[:, :, i] = torch.where(image==i, ones, zeros)
+
+		void_ids = [0, 1, 2, 3, 4, 5, 6, 42, 43, 44]
+		flat_ids = [7, 8, 9, 10]
+		construction_ids = [11, 12, 13, 14, 15, 16, 34, 35, 36]
+		object_ids = [17, 18, 19, 20, 21, 22, 37, 38, 39, 40, 41]
+		nature_ids = [21, 22]
+		sky_ids = [23]
+		human_ids = [24, 25]
+		vehicle_ids = [26, 27, 28, 29, 30, 31, 32, 33]
+
+		voids = binary_classification[void_ids]
+		flats = binary_classification[flat_ids]
+		constructions = binary_classification[construction_ids]
+		objects = binary_classification[object_ids]
+		natures = binary_classification[nature_ids]
+		sky = binary_classification[sky_ids]
+		humans = binary_classification[human_ids]
+		vehicles = binary_classification[vehicle_ids]
+
+		return {
+					"voids": voids,
+					"flats": flats,
+					"constructions": constructions,
+					"objects": objects,
+					"natures": natures,
+					"sky": sky,
+					"humans": humans,
+					"vehicles": vehicles
+		}
+
+
+class Kitti360ClassesBuilder(object):
+    def __init__(self):
+        self._instance = None
+
+    def __call__(self, data_dir: str, crop_size: int, sample_size: int = None, **_ignored):
+
+        self._instance = Kitti360Classes(
+            data_dir=data_dir,
+            sample_size=sample_size,
+            crop_size=crop_size
+        )
+        return self._instance
